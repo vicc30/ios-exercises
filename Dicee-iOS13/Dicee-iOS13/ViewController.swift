21312933898bbb7abc7b3172ebd2777cbd5853f2
Victor Cruz <victor.cruz@globant.com>
2022-05-16 14:42:32 -0500

    Add dicee app

--- Dicee-iOS13/Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/Dicee-iOS13/ViewController.swift
@@ -8,13 +8,24 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+  // IBOutlet allows me to reference a storyboard element.
+  @IBOutlet weak var diceImageViewOne: UIImageView!
+  @IBOutlet weak var diceImageViewTwo: UIImageView!
 
+  // Interface builder action
+  @IBAction func rollButtonPressed(_ sender: UIButton) {
 
+    //constant array
+    let diceArr = [ #imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix") ]
+
+    // randomElement gives a random element of array
+    diceImageViewOne.image = diceArr.randomElement()
+
+    // we need to give a range when we use Int.random(in: range)
+    diceImageViewTwo.image = diceArr[Int.random(in: 0...5)]
+
+  }
 }
 
