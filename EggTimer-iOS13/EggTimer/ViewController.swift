7b4ffd61b47ab30c7650fc303ae98ccb814e837e
Victor Cruz <victor.cruz@globant.com>
2022-05-17 11:11:38 -0500

    Add Egg timer solution

--- EggTimer-iOS13/EggTimer/ViewController.swift
+++ EggTimer-iOS13/EggTimer/ViewController.swift
@@ -8,8 +8,45 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-    
+final class ViewController: UIViewController {
 
+  @IBOutlet weak var titleText: UILabel!
+  @IBOutlet weak var timerBar: UIProgressView!
 
+  let eggTime: [String: Float] = [
+    "Soft": 3.0,
+    "Medium": 4.0,
+    "Hard": 7.0
+  ]
+
+  var secondsRemain: Float = 60
+  var totalTime: Float = 0
+  var timer: Timer = Timer()
+
+  @IBAction func hardnessSelected(_ sender: UIButton) {
+
+    guard let hardness: String = sender.currentTitle else {
+      return
+    }
+    timerBar.progress = 0.0
+    titleText.text = "How do you like your eggs?"
+    totalTime = eggTime[hardness] ?? 720
+    secondsRemain = 0
+    countDown()
+  }
+
+  func countDown() -> Void {
+    timer.invalidate()
+    timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+  }
+
+  @objc func updateTimer(){
+    if(secondsRemain < totalTime) {
+      secondsRemain += 1
+      timerBar.progress = secondsRemain/totalTime
+    } else {
+      titleText.text = "Your eggs are done!"
+      timer.invalidate()
+    }
+  }
 }
