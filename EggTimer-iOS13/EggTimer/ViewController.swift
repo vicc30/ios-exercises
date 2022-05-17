29517d00c0fd5aab5a192e6a31391028dbd7d957
Victor Cruz <victor.cruz@globant.com>
2022-05-17 11:20:08 -0500

    Add play sound

--- EggTimer-iOS13/EggTimer/ViewController.swift
+++ EggTimer-iOS13/EggTimer/ViewController.swift
@@ -7,11 +7,13 @@
 //
 
 import UIKit
+import AVFoundation
 
 final class ViewController: UIViewController {
 
   @IBOutlet weak var titleText: UILabel!
   @IBOutlet weak var timerBar: UIProgressView!
+  var player: AVAudioPlayer!
 
   let eggTime: [String: Float] = [
     "Soft": 3.0,
@@ -47,6 +49,13 @@ final class ViewController: UIViewController {
     } else {
       titleText.text = "Your eggs are done!"
       timer.invalidate()
+      playSound()
     }
+  }
+
+  func playSound() {
+    let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3")
+    player = try! AVAudioPlayer(contentsOf: url!)
+    player.play()
   }
 }
