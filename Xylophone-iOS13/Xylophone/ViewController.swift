ebcac032395c01cfb056b52cc0461569e1d80934
Victor Cruz <victor.cruz@globant.com>
2022-05-16 20:20:21 -0500

    Add Xylophone final project and challenge

--- Xylophone-iOS13/Xylophone/ViewController.swift
+++ Xylophone-iOS13/Xylophone/ViewController.swift
@@ -1,21 +1,29 @@
-//
-//  ViewController.swift
-//  Xylophone
-//
-//  Created by Angela Yu on 28/06/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
-//
-
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+  var player: AVAudioPlayer!
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+
+  @IBAction func keyPressed(_ sender: UIButton) {
+    guard let title = sender.currentTitle else {
+      return
     }
+    sender.alpha = 0.5
+    DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+      sender.alpha = 1
+    }
+    playSound(letter: title)
+  }
 
+  func playSound(letter: String) {
+    let url = Bundle.main.url(forResource: letter, withExtension: "wav")
+    player = try! AVAudioPlayer(contentsOf: url!)
+    player.play()
 
-    
-
+  }
 }
-
