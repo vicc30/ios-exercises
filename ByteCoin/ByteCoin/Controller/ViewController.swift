0145d2874a39b621a2381dc87b719799bb0d5697
Victor Cruz <victor.cruz@globant.com>
2022-05-31 12:35:22 -0500

    Add UIPickView delegate

--- ByteCoin/ByteCoin/Controller/ViewController.swift
+++ ByteCoin/ByteCoin/Controller/ViewController.swift
@@ -8,13 +8,39 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+//MARK: -  ViewController
+final class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+  @IBOutlet weak var currencyLabel: UILabel!
+  @IBOutlet weak var bitcoinLabel: UILabel!
+  @IBOutlet weak var currencyPicker: UIPickerView!
 
+  let coinManager = CoinManager()
 
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    currencyPicker.dataSource = self
+    currencyPicker.delegate = self
+  }
+
+  //MARK: -  UIPickerViewDataSource
+  func numberOfComponents(in pickerView: UIPickerView) -> Int {
+    return 1
+  }
+
+  func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+    return coinManager.currencyArray.count
+  }
+
+  //MARK: -  UIPickerViewDelegate
+  func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+    return coinManager.currencyArray[row]
+  }
+
+  func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+    let currency: String = coinManager.currencyArray[row]
+    coinManager.getCoinPrice(for: currency)
+  }
 }
 
