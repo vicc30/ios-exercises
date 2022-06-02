dba787c81ad9b7f5d29e4560180c7a23c832dc10
Victor Cruz <victor.cruz@globant.com>
2022-06-02 18:32:16 -0500

    add view controller

--- Flash-Chat/Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash-Chat/Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -10,13 +10,23 @@ import UIKit
 
 class WelcomeViewController: UIViewController {
 
-    @IBOutlet weak var titleLabel: UILabel!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
+  @IBOutlet weak var titleLabel: UILabel!
 
-       
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    titleLabel.text = ""
+    var charIndex: Double = 0
+    let titleText = "⚡️FlashChat"
+
+    for letter in titleText {
+      Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex, repeats: false) { (timer) in
+        self.titleLabel.text?.append(letter)
+      }
+      charIndex += 1
     }
-    
+
+  }
+
 
 }
