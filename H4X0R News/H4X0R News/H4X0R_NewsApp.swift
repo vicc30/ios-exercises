a1adb95830ade678a0e94c718e1c376db314d742
Victor Cruz <victor.cruz@globant.com>
2022-06-03 10:29:39 -0500

    H4X0R News initial project

---
+++ H4X0R News/H4X0R News/H4X0R_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4X0R_NewsApp.swift
+//  H4X0R News
+//
+//  Created by Victor Cruz Galindo on 03/06/22.
+//
+
+import SwiftUI
+
+@main
+struct H4X0R_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
